891e952723d9ea2b350eb9f71f81d736723329bd
zhaozhicheng <2363402697@qq.com>
2019-03-20 16:48:51 +0800

    111

--- TrainingManagement/settings.py
+++ TrainingManagement/settings.py
@@ -115,6 +115,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = 'index'
+LOGIN_URL = 'login'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
