684f8a43c4814c2a9bc8cacd2dda846f6747d666
zhaozhicheng <2363402697@qq.com>
2019-03-20 17:18:46 +0800

    111

--- training/urls.py
+++ training/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views as user_views
+
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('',user_views.index,name='index'),
+    path('login/',auth_views.LoginView.as_view(),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
