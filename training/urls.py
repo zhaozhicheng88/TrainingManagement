82e43837cad60c74bcfe486a71a0f239b9904546
zhaozhicheng <2363402697@qq.com>
2019-03-20 16:51:36 +0800

    111

--- training/urls.py
+++ training/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views as user_views
 
 urlpatterns = [
-    path('', user_views.index, name='index'),
+    # path('', user_views.index, name='index'),
     path('', include('django.contrib.auth.urls')),
 
 ]
