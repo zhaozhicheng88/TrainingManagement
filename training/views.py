9ec2ec5ceb18c048519df63fe1b972586375240b
zhaozhicheng <2363402697@qq.com>
2019-03-20 17:23:23 +0800

    111

--- training/views.py
+++ training/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+
 
 # Create your views here.
+@login_required
 def index(request):
-    return render(request,'training/index.html')
+    profile = get_object_or_404(Profile, user=request.user)
+    return render(request, 'training/index.html', {'profile': profile})
